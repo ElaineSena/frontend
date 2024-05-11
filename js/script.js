7a881e716785abbbea1209fb4883da18ce8700dc
ElaineSena <167260937+ElaineSena@users.noreply.github.com>
2024-05-10 21:18:17 -0300

    update change theme att

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const toggleTheme = document.getElementById("toggleTheme"); 
+const rootHtml = document.documentElement;
+const accordionHeaders = document.querySelectorAll(".accordion-header"); // Seleciona todos os elementos HTML que possuem a classe como o mesmo nome
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function changeTheme(){ // Altera o tema da interface entre claro e escuro
+    const currentTheme = rootHtml.getAttribute("data-theme");
+
+    currentTheme === "dark" ? rootHtml.setAttribute("data-theme", "light") : rootHtml.setAttribute("data-theme", "dark");
+
+    toggleTheme.classList.toggle("bi-sun");
+    toggleTheme.classList.toggle("bi-moon-stars");
+}
+
+toggleTheme.addEventListener("click", changeTheme);
+
+
